97eb8b525ad5ba212e75b1fa46008f323d4262ed
Debmalya Jash <debmalya.jash@gmail.com>
2019-09-29 17:26:00 +0800

    Adding text for movement

--- app/src/main/java/org/deb/widget/controller/SwipeButtonController.kt
+++ app/src/main/java/org/deb/widget/controller/SwipeButtonController.kt
@@ -1,7 +1,10 @@
 package org.deb.widget.controller
 
+import android.app.Notification
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
@@ -14,18 +17,29 @@ class SwipeButtonController : AppCompatSeekBar {
     private var confirmed: Boolean = false
     private var confirm: String = "Confirm"
     private var reject: String = "Reject"
+    private var paint = Paint()
+    private var canvas = Canvas()
+    private var used : Boolean = false
 
     fun setPermissible(limit: Int) {
         permissiable = limit
     }
 
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
-    )
+    ) {
+        init()
+    }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
 
@@ -33,25 +47,25 @@ class SwipeButtonController : AppCompatSeekBar {
             if (thumb.bounds.contains(event.x.toInt(), event.y.toInt())) {
                 parent.requestDisallowInterceptTouchEvent(true)
                 super.onTouchEvent(event)
-            }  else {
+            } else {
                 return false
             }
         } else if (event.action == MotionEvent.ACTION_UP) {
             if (progress > permissiable && !confirmed) {
                 // Confirm
-                Log.d("SWIPE :", "CONFIRMED")
-                setBackgroundColor(Color.rgb(50,200,50))
+                setBackgroundColor(Color.rgb(50, 200, 50))
                 confirmed = true
+                used = true
+//                canvas?.drawText(confirm, this.x+this.width/2 , this.y +this.height/2, paint)
             } else if (progress < permissiable && confirmed) {
                 // assumption: once confirmed then only can be rejected
                 // default mode is rejected
-                Log.d("SWIPE :", "REJECTED")
-                setBackgroundColor(Color.rgb(200,50,70))
-//                progress = 0
+                setBackgroundColor(Color.rgb(200, 50, 70))
                 confirmed = false
+//                canvas?.drawText(reject, this.x+this.width/2 , this.y +this.height/2, paint)
             }
             parent.requestDisallowInterceptTouchEvent(false)
-            progress = if (confirmed){
+            progress = if (confirmed) {
                 100
             } else {
                 0
@@ -60,6 +74,27 @@ class SwipeButtonController : AppCompatSeekBar {
             super.onTouchEvent(event)
         }
         return true
+    }
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        if (used) {
+            var message: String = reject
+            if (confirmed) {
+                message = confirm
+            }
+            canvas?.drawText(message, this.x + this.width / 3, this.y + this.height / 2, paint)
+        }
+    }
+
+
+    private fun init() {
+        paint.color = Color.WHITE
+        paint.style = Paint.Style.FILL
+//        paint.textAlign= p
+        paint.textSize = 100f;
+
     }
 
 }
